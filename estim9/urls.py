d2acbd36c787268273035918139870353ec6a0a8
8one6 <jbrodie@8one6.com>
2013-05-28 17:03:55 -0400

    Should give blank page now

--- estim9/urls.py
+++ estim9/urls.py
@@ -1,17 +1,22 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from estim9.views import hello
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'estim9.views.home', name='home'),
     # url(r'^estim9/', include('estim9.foo.urls')),
+	url(r'^hello/$', hello),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
