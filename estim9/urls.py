62c4c8c33fc27d1ab25bb1f6e44369dbd34e1017
8one6 <jbrodie@8one6.com>
2013-05-31 11:55:26 -0400

    Added pygraph and some tests.  Added responsive CSS

--- estim9/urls.py
+++ estim9/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from estim9.views import hello
+from graph.views import gvtest
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,6 +12,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'estim9.views.home', name='home'),
     # url(r'^estim9/', include('estim9.foo.urls')),
 	url(r'^hello/$', hello),
+	url(r'^gvtest/$', gvtest),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- graph/views.py
+++ graph/views.py
@@ -1,1 +1,24 @@
 # Create your views here.
+import pydot
+from django.shortcuts import render
+
+def gvtest(request):
+	graph = pydot.Dot(graph_type='graph')
+
+	numlords = 3
+	numvassalsplord = 3
+	for i in range(numlords):
+		edge = pydot.Edge("king", "lord%d" % i)
+		graph.add_edge(edge)
+
+	vassal_num = 0
+	for i in range(numlords):
+		for j in range(numvassalsplord):
+			edge = pydot.Edge("lord%d" % i, "vassal%d" % vassal_num)
+			graph.add_edge(edge)
+			vassal_num+=1
+	
+	svg = graph.create_svg()
+	mydict = {}
+	mydict['content'] = svg
+	return render(request, 'base.html', mydict)
