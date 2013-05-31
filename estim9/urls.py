1ef735bb30952f08bc3d6bdf97c8eaf14d5ba9c9
8one6 <jbrodie@8one6.com>
2013-05-31 15:16:50 -0400

    Added tests for dynamic graph rendering

--- estim9/urls.py
+++ estim9/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from estim9.views import hello
-from graph.views import gvtest
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,8 +9,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'estim9.views.home', name='home'),
     # url(r'^estim9/', include('estim9.foo.urls')),
-	url(r'^hello/$', hello),
-	url(r'^gvtest/$', gvtest),
+	url(r'^hello/$', 'estim9.views.hello'),
+	url(r'^gvtest/$', 'graph.views.gvtest'),
+	url(r'^gvtest2/$', 'graph.views.gvtest2'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- graph/views.py
+++ graph/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 import pydot
 from django.shortcuts import render
+from graph.models import Graph, Edge, Node
 
 def gvtest(request):
 	graph = pydot.Dot(graph_type='graph')
@@ -22,3 +23,46 @@ def gvtest(request):
 	mydict = {}
 	mydict['content'] = svg
 	return render(request, 'base.html', mydict)
+	
+def gvtest2(request, graph_id=1):
+	mydict = {}
+	
+	graph_qs = Graph.objects.get(pk=graph_id)
+	graph = pydot.Dot(
+		graph_name=unicode(graph_qs),
+		graph_type='digraph')
+	mydict['graph_name'] = graph_qs.name
+	
+	nodes_qs = Node.objects.filter(graph=graph_id)
+	nodes = []
+	for n in nodes_qs:
+		nodes.append(unicode(n))
+		g_n = pydot.Node(
+			n.pk, 
+			id = unicode(n),
+			label=unicode(n), 
+			URL='http://www.nytimes.com/%s' % n.pk)
+		graph.add_node(g_n)
+	if nodes:
+		mydict['nodes'] = nodes
+	
+	edges_qs = Edge.objects.filter(graph=graph_id)
+	edges = []
+	for e in edges_qs:
+		edges.append(unicode(e))
+		g_e = pydot.Edge(
+			e.source.pk, 
+			e.dest.pk, 
+			id = unicode(e.source) + '->' + unicode(e.dest),
+			#label=unicode(e), 
+			URL='http://www.wsj.com/%s' % e.pk)
+		graph.add_edge(g_e)
+	if edges:
+		mydict['edges'] = edges
+		
+	svg = graph.create_svg()
+	if (nodes or edges):
+		mydict['svg'] = svg
+	
+	return render(request, 'graphview.html', mydict)
+	
