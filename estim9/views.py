5560c831d6333db2db6f79bedf3b2a71d83cc999
8one6 <jbrodie@8one6.com>
2013-06-02 19:11:52 -0400

    Refactor home page template

--- estim9/views.py
+++ estim9/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-def hello(request):
-	return render(request, 'base.html', {})
-
 def home(request):
-	return render(request, 'base.html', {})
+	return render(request, 'home.html', {})
