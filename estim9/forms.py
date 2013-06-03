c501e4bd8628f639c3dfe5f14d351ac5ea772962
8one6 <jbrodie@8one6.com>
2013-06-03 14:37:05 -0400

    Improve login error messages

--- estim9/forms.py
+++ estim9/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class LoginForm(forms.Form):
-	username 		= forms.CharField(max_length=100)
-	password		= forms.CharField(max_length=100, widget=forms.PasswordInput)
+	username 		= forms.CharField(max_length=100, label=u'Username')
 	username.widget.attrs['placeholder'] = u'Username'
+	password		= forms.CharField(max_length=100, label=u'Password', widget=forms.PasswordInput)
 	password.widget.attrs['placeholder'] = u'Password'
--- estim9/views.py
+++ estim9/views.py
@@ -1,12 +1,24 @@
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from estim9.forms import LoginForm
 	
 def home(request):
-	return render(request, 'home.html', {})
+	return render(request, 'home.html')
 	
 def login(request):
-	if request.method == 'POST':
-		pass
-	else:
-		form = LoginForm()
-		return render(request, 'account/login.html', { 'login_form':form })
+	context = {}
+	
+	if request.user.is_authenticated(): #already logged in
+		return HttpResponseRedirect('/account/profile')
+	
+	form = LoginForm(request.POST or None)
+	context['login_form'] = form
+	
+	if request.method == 'POST': #submitting form
+		if form.is_valid():
+			context['content'] = form.cleaned_data
+			return render(request, 'test.html', context)
+		else: #invalid form
+			return render(request, 'account/login.html', context)
+	else:  #display empty form
+		return render(request, 'account/login.html', context)
