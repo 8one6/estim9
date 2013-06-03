2c15162e7e6bdc71f23ad25ed0a0dd44f30a8921
8one6 <jbrodie@8one6.com>
2013-06-03 01:58:33 -0400

    Use django form for login

--- estim9/views.py
+++ estim9/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from estim9.forms import LoginForm
+	
 def home(request):
 	return render(request, 'home.html', {})
 	
 def login(request):
-	return render(request, 'account/login.html', {})
+	if request.method == 'POST':
+		pass
+	else:
+		form = LoginForm()
+		return render(request, 'account/login.html', { 'login_form':form })
