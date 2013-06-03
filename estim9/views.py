3668fe2e82b1fa11bf64004985d8acd1492e4b07
8one6 <jbrodie@8one6.com>
2013-06-03 16:26:31 -0400

    Finish login, add logout

--- estim9/views.py
+++ estim9/views.py
@@ -1,24 +1,43 @@
 from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from estim9.forms import LoginForm
 	
 def home(request):
 	return render(request, 'home.html')
-	
-def login(request):
-	context = {}
+
+def profile(request):
+	return render(request, 'account/profile.html')
+
+def login_user(request):
+	d = {}
 	
 	if request.user.is_authenticated(): #already logged in
 		return HttpResponseRedirect('/account/profile')
 	
 	form = LoginForm(request.POST or None)
-	context['login_form'] = form
+	d['login_form'] = form
 	
 	if request.method == 'POST': #submitting form
 		if form.is_valid():
-			context['content'] = form.cleaned_data
-			return render(request, 'test.html', context)
+			cd = form.cleaned_data
+			user = authenticate(username=cd['username'], password=cd['password'])
+			if user is not None:
+				if user.is_active: #all good, log user in
+					login(request, user)
+					return HttpResponseRedirect('/account/profile')
+				else: #account disabled
+					return HttpResponseRedirect('/account/disable')
+			else: #authenticate failed
+				errorlist = []
+				errorlist.append(u'Login failed, please try again.')
+				d['errors'] = errorlist
+				return render(request, 'account/login.html', d)
 		else: #invalid form
-			return render(request, 'account/login.html', context)
+			return render(request, 'account/login.html', d)
 	else:  #display empty form
-		return render(request, 'account/login.html', context)
+		return render(request, 'account/login.html', d)
+
+def logout_user(request):
+	logout(request)
+	return HttpResponseRedirect('/account/login')
